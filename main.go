7cf12bb28a7a8af775fa211302f1497be955c09d
Jesse Palmer <jesselpalmer@gmail.com>
2017-06-20 11:47:46 -0700

    feat: update welcome message

--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
+	var welcomeMessage = "Welcome to the Random APIs!"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "%s", welcomeMessage)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
