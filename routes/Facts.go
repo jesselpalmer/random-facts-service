29aa5d4718370ff1eff06d32e1bd76df25530ed4
Jesse Palmer <jesselpalmer@gmail.com>
2019-08-10 13:26:12 -0700

    refactor: seperate out data call

--- routes/Facts.go
+++ routes/Facts.go
@@ -7,13 +7,17 @@ import (
 	"github.com/jesselpalmer/random-facts-service/models/fact"
 )
 
+func getFacts() fact.Fact {
+	u := fact.Fact{Data: "The Unicorn is the national animal of Scotland",
+		Source: "http://www.scotsman.com/heritage/people-places/scottish-fact-of-the-week-scotland-s-official-animal-the-unicorn-1-2564399"}
+	return u
+}
+
 // Facts : facts data route
 func Facts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	u := fact.Fact{Data: "The Unicorn is the national animal of Scotland",
-		Source: "http://www.scotsman.com/heritage/people-places/scottish-fact-of-the-week-scotland-s-official-animal-the-unicorn-1-2564399"}
-
+	u := getFacts()
 	json.NewEncoder(w).Encode(u)
 }
